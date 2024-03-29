429328ddfdb3ea581f0963aca64f0adc5d71a575
tdss <dariusz.tarczynski@gmail.com>
2024-03-29 19:46:52 +0100

    chapter3.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+
+    let server = zero2prod::run(listener).await?;
+    let _ = tokio::spawn(server).await;
+    Ok(())
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,29 @@
+use std::net::TcpListener;
+
+async fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener)
+        .await
+        .expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+    println!("My lucky number is {}.", &address);
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
