4b4e715cb15fdc6ecc12c2eb974901c7308b8c00
tdss <dariusz.tarczynski@gmail.com>
2024-04-18 20:36:16 +0200

    chapter 4.5.11
    spent some good time on get_subscriber<Sink> function...

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ use zero2prod::telemetry::{init_subscriber, get_subscriber};
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
 
-    let subscriber = get_subscriber("zero2prod".into(), "into".into());
+    let subscriber = get_subscriber("zero2prod".into(), "into".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = zero2prod::configuration::get_configuration().expect("Failed to read configuration.");
     let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
-
+ 
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await.expect("Failed to connect to Postgres");
     let server = zero2prod::startup::run(listener, connection_pool).await?;
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,6 +6,23 @@ use sqlx::PgConnection;
 use sqlx::Executor;
 use zero2prod::configuration::get_configuration;
 use zero2prod::configuration::DatabaseSettings;
+use zero2prod::telemetry::{init_subscriber,get_subscriber};
+use once_cell::sync::Lazy;
+
+static TRACING: Lazy<()> = Lazy::new( || {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);    
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+
+    }
+
+});
 
 pub struct TestApp {
     pub address: String,
@@ -33,6 +50,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
